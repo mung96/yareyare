78ff62b4a52d79000bb9910d757ce813ab595b7f
mung96 <mung5079@gmail.com>
2024-10-25 09:20:12 +0900

    chore: emotion 설치

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,20 @@
-import {SafeAreaView, Text} from 'react-native';
+import styled from '@emotion/native';
+import {Text} from 'react-native';
+
+const Container = styled.View`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  font-size: 30px;
+  margin-top: 20px;
+`;
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
+    <Container>
       <Text>리액트 네이티브</Text>
-    </SafeAreaView>
+    </Container>
   );
 }
 
