2329c09bc549e555af629c1d09a7a397c9e85b9e
mung96 <mung5079@gmail.com>
2024-10-27 14:47:30 +0900

    refactor: 폴더구조 대분류 추가

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,8 +1,37 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-
+  extends: ['@react-native'],
+  plugins: ['import'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'import/no-restricted-paths': [
+      'error',
+      {
+        zones: [
+          {
+            target: './ui/**',
+            from: './ui/**',
+          },
+        ],
+      },
+    ],
+  },
+
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src/'],
+        extensions: ['.ts', '.tsx'],
+      },
+      //eslint 경로 읽을때 필요한 설정
+      alias: {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+        // map: [['@', './']],
+        // extensions: ['.ts', '.tsx'],
+      },
+    },
   },
 };
