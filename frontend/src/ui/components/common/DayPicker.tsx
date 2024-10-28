1ec5e944529ed66c1db4f0ffa57a8de4ad80aff4
mung96 <mung5079@gmail.com>
2024-10-28 15:38:47 +0900

    feat: 한글설정 구현

--- frontend/src/ui/components/common/DayPicker.tsx
+++ frontend/src/ui/components/common/DayPicker.tsx
@@ -4,6 +4,49 @@ import {useState} from 'react';
 
 type DateString = `${number}-${number}-${number}`; //YYYY-MM-DD
 
+LocaleConfig.locales.ko = {
+  monthNames: [
+    '01월',
+    '02월',
+    '03월',
+    '04월',
+    '05월',
+    '06월',
+    '07월',
+    '08월',
+    '09월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  monthNamesShort: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  dayNames: [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일',
+  ],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  today: '오늘',
+};
+LocaleConfig.defaultLocale = 'ko';
+
 function DayPicker() {
   const [date, setDate] = useState<DateString | undefined>();
 
