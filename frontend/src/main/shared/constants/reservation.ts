da2b45e7a82677d77a479cb7cfc6c568b9370540
mung96 <mung5079@gmail.com>
2024-10-30 10:30:32 +0900

    feat: section 이미지 등록

--- frontend/src/main/shared/constants/reservation.ts
+++ frontend/src/main/shared/constants/reservation.ts
@@ -1,9 +1,37 @@
 import {COLORS} from '@/main/shared';
 
+const SECTION_IMG = {
+  FIRST_INFIELD: require('@/main/assets/firstInfield.png'),
+  FIRST_OUTFIELD: require('@/main/assets/firstoutfield.png'),
+  CENTER: require('@/main/assets/center.png'),
+  THIRD_INFIELD: require('@/main/assets/thirdInfield.png'),
+  THIRD_OUTFIELD: require('@/main/assets/thirdOutfield.png'),
+};
+
 export const SECTION = {
-  FIRST_INFIELD: {label: '1루 내야', color: COLORS.BLUE},
-  FIRST_OUTFIELD: {label: '1루 외야', color: COLORS.GREEN},
-  MIDDLE: {label: '중앙테이블석', color: COLORS.PINK},
-  THIRD_INFIELD: {label: '3루 내야', color: COLORS.BLUE},
-  THIRD_OUTFIELD: {label: '3루 외야', color: COLORS.GREEN},
+  FIRST_INFIELD: {
+    label: '1루 내야',
+    color: COLORS.BLUE,
+    img: SECTION_IMG.FIRST_INFIELD,
+  },
+  FIRST_OUTFIELD: {
+    label: '1루 외야',
+    color: COLORS.GREEN,
+    img: SECTION_IMG.FIRST_OUTFIELD,
+  },
+  CENTER: {
+    label: '중앙테이블석',
+    color: COLORS.PINK,
+    img: SECTION_IMG.CENTER,
+  },
+  THIRD_INFIELD: {
+    label: '3루 내야',
+    color: COLORS.BLUE,
+    img: SECTION_IMG.THIRD_INFIELD,
+  },
+  THIRD_OUTFIELD: {
+    label: '3루 외야',
+    color: COLORS.GREEN,
+    img: SECTION_IMG.THIRD_OUTFIELD,
+  },
 };
