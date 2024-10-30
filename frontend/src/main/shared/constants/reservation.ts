6f5dae6f415b68891285172a3f588a727a6a0749
mung96 <mung5079@gmail.com>
2024-10-30 10:23:42 +0900

    feat: section 상수 구현

---
+++ frontend/src/main/shared/constants/reservation.ts
@@ -0,0 +1,9 @@
+import {COLORS} from '@/main/shared';
+
+export const SECTION = {
+  FIRST_INFIELD: {label: '1루 내야', color: COLORS.BLUE},
+  FIRST_OUTFIELD: {label: '1루 외야', color: COLORS.GREEN},
+  MIDDLE: {label: '중앙테이블석', color: COLORS.PINK},
+  THIRD_INFIELD: {label: '3루 내야', color: COLORS.BLUE},
+  THIRD_OUTFIELD: {label: '3루 외야', color: COLORS.GREEN},
+};
