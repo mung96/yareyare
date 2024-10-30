fb1893d4f5fcc221d897103a0a3893ae4480c066
mung96 <mung5079@gmail.com>
2024-10-30 15:06:21 +0900

    refactor: 유저 정보 입력란 파일 분리

--- frontend/src/main/apps/screens/reservationProcess/UserScreen.tsx
+++ frontend/src/main/apps/screens/reservationProcess/UserScreen.tsx
@@ -1,8 +1,8 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
-import {COLORS} from '@/main/shared/styles';
-import InputField from '@/main/ui/components/common/InputField.tsx';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import MethodSelector from '@/main/ui/components/reservation/MethodSelector.tsx';
+import UserInput from '@/main/ui/components/reservation/UserInput.tsx';
+import MainButton from '@/main/ui/widgets/MainButton.tsx';
 
 type Props = {
   onPrev: () => void;
@@ -12,25 +12,9 @@ type Props = {
 function UserScreen({onPrev, onNext}: Props) {
   return (
     <View style={styles.container}>
-      <View style={styles.componentContainer}>
-        <MethodSelector />
-        <View style={styles.titleContainer}>
-          <Text style={styles.categoryText}>일반</Text>
-          <Text style={styles.totalPrice}>15,000원</Text>
-        </View>
-        <Text style={styles.inputContainerTitle}>예매자 정보 입력</Text>
-        <View style={styles.inputContainer}>
-          <InputField label={'성명'} value={'정지연'} disabled={true} />
-          <InputField label={'생년월일'} value={'000315'} disabled={true} />
-          <InputField label={'전화번호'} placeholder={'000-0000-0000'} />
-          <InputField label={'이메일주소'} placeholder={'example@naver.com'} />
-          <Text style={styles.description}>
-            티켓수령 및 본인확인을 위해 정확한 정보를 입력해주세요
-          </Text>
-        </View>
-      </View>
-
-      <Button title={'다음'} onPress={onNext} />
+      <UserInput />
+      <MethodSelector />
+      <MainButton label={'다음'} onPress={onNext} size={'large'} />
     </View>
   );
 }
@@ -40,44 +24,6 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'column',
     gap: 10,
-  },
-  titleContainer: {
-    borderBottomWidth: 0.7,
-    borderColor: COLORS.BLACK,
-    paddingVertical: 14,
-  },
-  categoryText: {
-    color: COLORS.GRAY_300,
-    fontSize: 12,
-  },
-  totalPrice: {
-    fontSize: 20,
-    color: COLORS.BLACK,
-    fontWeight: '900',
-  },
-  description: {
-    fontSize: 12,
-    color: COLORS.GRAY_300,
-  },
-
-  componentContainer: {
-    backgroundColor: COLORS.GRAY_400,
-    paddingHorizontal: 14,
-    paddingVertical: 14,
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 12,
-  },
-  inputContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 6,
-    paddingHorizontal: 12,
-  },
-  inputContainerTitle: {
-    fontSize: 14,
-    color: COLORS.BLACK,
-    fontWeight: '900',
   },
 });
 
---
+++ frontend/src/main/ui/components/reservation/UserInput.tsx
@@ -0,0 +1,67 @@
+import {StyleSheet, Text, View} from 'react-native';
+import InputField from '@/main/ui/components/common/InputField.tsx';
+import React from 'react';
+import {COLORS} from '@/main/shared/styles';
+
+function UserInput() {
+  return (
+    <View style={styles.componentContainer}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.categoryText}>일반</Text>
+        <Text style={styles.totalPrice}>15,000원</Text>
+      </View>
+      <Text style={styles.inputContainerTitle}>예매자 정보 입력</Text>
+      <View style={styles.inputContainer}>
+        <InputField label={'성명'} value={'정지연'} disabled={true} />
+        <InputField label={'생년월일'} value={'000315'} disabled={true} />
+        <InputField label={'전화번호'} placeholder={'000-0000-0000'} />
+        <InputField label={'이메일주소'} placeholder={'example@naver.com'} />
+        <Text style={styles.description}>
+          티켓수령 및 본인확인을 위해 정확한 정보를 입력해주세요
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    borderBottomWidth: 0.7,
+    borderColor: COLORS.BLACK,
+    paddingVertical: 14,
+  },
+  categoryText: {
+    color: COLORS.GRAY_300,
+    fontSize: 12,
+  },
+  totalPrice: {
+    fontSize: 20,
+    color: COLORS.BLACK,
+    fontWeight: '900',
+  },
+  description: {
+    fontSize: 12,
+    color: COLORS.GRAY_300,
+  },
+
+  componentContainer: {
+    backgroundColor: COLORS.GRAY_400,
+    paddingHorizontal: 14,
+    paddingVertical: 14,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 12,
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 6,
+    paddingHorizontal: 12,
+  },
+  inputContainerTitle: {
+    fontSize: 14,
+    color: COLORS.BLACK,
+    fontWeight: '900',
+  },
+});
+export default UserInput;
