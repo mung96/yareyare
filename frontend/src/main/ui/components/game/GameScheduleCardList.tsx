ace2ca78951c721f33aba7e90039315eaa8d7e14
mung96 <mung5079@gmail.com>
2024-10-31 17:20:36 +0900

    feat: 예매하기 눌러서 예매페이지 가는 기능 구현

--- frontend/src/main/ui/components/game/GameScheduleCardList.tsx
+++ frontend/src/main/ui/components/game/GameScheduleCardList.tsx
@@ -1,10 +1,13 @@
 import {StyleSheet, View} from 'react-native';
 import GameScheduleCard from '@/main/ui/components/game/GameScheduleCard.tsx';
 import {TEAM_LIST} from '@/main/shared/constants/team.ts';
+import {useDispatch} from 'react-redux';
+import {moveNavigation} from '@/main/stores/clients/navigationCategory.ts';
 
 function GameScheduleCardList() {
   //TODO: 경기일정 하나씩 불러오면됨
   //TODO: Slider 구현
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.container}>
@@ -12,7 +15,7 @@ function GameScheduleCardList() {
         dateTime={'24.08.15(목)17:00'}
         awayTeam={TEAM_LIST.DOOSAN}
         homeTeam={TEAM_LIST.NC}
-        onPress={() => console.log('예매버튼 클릭')}
+        onPress={() => dispatch(moveNavigation('reservation'))}
       />
       <GameScheduleCard
         dateTime={'24.08.15(목)17:00'}
