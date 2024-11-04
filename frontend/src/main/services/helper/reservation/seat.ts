3dd9d200c2e639e5a92f6eb09a35f696ca9aff63
mung96 <mung5079@gmail.com>
2024-11-04 17:14:57 +0900

    feat: dummy 데이터 생성

--- frontend/src/main/services/helper/reservation/seat.ts
+++ frontend/src/main/services/helper/reservation/seat.ts
@@ -1,5 +1,17 @@
 import {Seat} from '@/main/apps/screens/reservationProcess';
 import {includeObjectWithKeyAndValue} from '@/main/shared/utils/array.ts';
+import {
+  convertCharToNumber,
+  convertNumberToChar,
+} from '@/main/shared/utils/type.ts';
+
+export function convertRowToIdx(row: string) {
+  return convertCharToNumber(row) - 65;
+}
+
+export function convertIdxToRow(idx: number) {
+  return convertNumberToChar(row + 65);
+}
 
 export function includeSeatWithRowAndCol(
   seatList: Seat[],
@@ -8,3 +20,42 @@ export function includeSeatWithRowAndCol(
 ) {
   return includeObjectWithKeyAndValue(seatList, ['row', 'col'], [row, col]);
 }
+
+const row = 'K'.charCodeAt(0) - 'A'.charCodeAt(0);
+const col = 20;
+
+export function convertSeatResponseToData(response: {
+  [section: number]: {
+    [row: string]: {
+      [col: number]: boolean;
+    };
+  };
+}) {
+  const seatList: boolean[][] = Array.from({length: row}, () =>
+    Array(col).fill(false),
+  );
+
+  // Object.keys(response).map(sectionStr => {
+  //   const section = Number(sectionStr);
+  //   Object.keys(response[section]).map(row =>
+  //     response[section][row].map(
+  //       seat =>
+  //         (seatList[row.charCodeAt(0) - 65][seat.col - 1] = seat.available),
+  //     ),
+  //   );
+  // });
+
+  return seatList;
+}
+
+type TSeatList = {
+  number: boolean[][];
+};
+
+// class SeatList {
+//   readonly items: TSeatList;
+//
+//   // constructor() {
+//   //   this.items = :
+//   // }
+// }
