56dbf680a5c70429fa62e8a195dba497dcde784e
이윤지 <yunji383614@gmail.com>
2024-11-04 10:32:56 +0900

    feat : Fcm Entity 생성

---
+++ backend/member_service/src/main/java/yare/yare/domain/fcm/entity/Fcm.java
@@ -0,0 +1,32 @@
+package yare.yare.domain.fcm.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import yare.yare.domain.member.entity.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Fcm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fcm_id", columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @NotNull
+    @Column(length = 50, unique = true)
+    private String token;
+
+    @NotNull
+    private Boolean approved;
+}
