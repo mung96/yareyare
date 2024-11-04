23d2d8bea94b3030c09ce662f3a2f8982ebd86b5
Cutaku <mmzz0219@gmail.com>
2024-11-04 20:47:41 +0900

    feat : 최근 경기 결과 조회 mock api 구현

--- backend/game-service/src/main/java/yare/yare/domain/game/dto/LastGameListRes.java
+++ backend/game-service/src/main/java/yare/yare/domain/game/dto/LastGameListRes.java
@@ -1,11 +1,13 @@
 package yare.yare.domain.game.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@Builder
 public class LastGameListRes {
 
     private LocalDate gameDate;
