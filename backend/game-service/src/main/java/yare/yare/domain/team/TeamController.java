16f973b99314d1712476a048b76cb654b34c6ede
Cutaku <mmzz0219@gmail.com>
2024-11-05 10:55:33 +0900

    feat : 공통 url 추가

--- backend/game-service/src/main/java/yare/yare/domain/team/TeamController.java
+++ backend/game-service/src/main/java/yare/yare/domain/team/TeamController.java
@@ -1,6 +1,7 @@
 package yare.yare.domain.team;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yare.yare.domain.team.dto.TeamDto;
 import yare.yare.domain.team.dto.TeamListRes;
@@ -12,6 +13,7 @@ import java.util.List;
 import static yare.yare.global.statuscode.SuccessCode.OK;
 
 @RestController
+@RequestMapping("/games")
 public class TeamController {
 
     @GetMapping("/teams")
