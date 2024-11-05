ea9b55ee45104e2c6fbef5e7f4ad121ea93fd2c0
이윤지 <yunji383614@gmail.com>
2024-11-05 10:26:15 +0900

    feat : Redis 관련 설정 추가

---
+++ backend/member_service/src/main/java/yare/yare/global/utils/RedisUtils.java
@@ -0,0 +1,27 @@
+package yare.yare.global.utils;
+
+import java.util.List;
+import java.util.Set;
+
+public interface RedisUtils {
+
+    void setData(String key, Object value);
+
+    void setDataWithExpiration(String key, Object value, Long expiredTime);
+
+    Object getData(String key);
+
+    void addListData(String key, Object value);
+
+    List<Object> getListData(String key);
+
+    void deleteData(String key);
+
+    Set<String> keys(String pattern);
+
+    Boolean lock(String key, Long timeout);
+
+    Boolean lock(String key, String value, Long expireTime);
+
+    void unlock(String key);
+}
