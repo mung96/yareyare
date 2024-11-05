45cb8121284c82bbfb93d1912d9da17207912079
mung96 <mung5079@gmail.com>
2024-11-05 13:20:53 +0900

    feat: 배열에서 item 추가, 삭제하는 함수 일급으로 변경

--- frontend/src/main/shared/utils/array.ts
+++ frontend/src/main/shared/utils/array.ts
@@ -7,3 +7,15 @@ export function includeObjectWithKeyAndValue<T>(
     keys.every((key, index) => obj[key] === values[index]),
   );
 }
+
+export function addItemToArray<T>(arr: Array<T>, item: T) {
+  return [...arr, item];
+}
+
+export function removeItemFromArray<T>(
+  arr: Array<T>,
+  item: T,
+  condition: (item: T) => boolean,
+) {
+  return arr.filter(item => condition(item));
+}
