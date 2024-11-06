4bbe385858541021d9b0554ab41669f591bcdbe3
mung96 <mung5079@gmail.com>
2024-11-06 17:40:00 +0900

    feat: game관련  endpoint 작성

---
+++ frontend/src/main/shared/constants/api.ts
@@ -0,0 +1,11 @@
+export const END_POINT = {
+  //game
+  GAME: 'games',
+  GAME_RESULT: 'games/results',
+  GAME_PLAN: (teamId: string) => `games/teams/${teamId}`,
+  GAME_SCHEDULE: (teamId: string) => `games/teams/${teamId}/schedule`,
+
+  GAME_DETAIL: (gameId: string) => `games/${gameId}/details`,
+  GAME_GRADE: (gameId: string) => `games/${gameId}/grades`,
+  GAME_SEAT: (gameId: string) => `games/${gameId}/seats`,
+};
