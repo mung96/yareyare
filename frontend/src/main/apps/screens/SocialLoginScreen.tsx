befdbaa66b1a81bca103750c552af6b591b07eae
mung96 <mung5079@gmail.com>
2024-11-06 11:19:03 +0900

    feat: encryptStorage util 구현

--- frontend/src/main/apps/screens/SocialLoginScreen.tsx
+++ frontend/src/main/apps/screens/SocialLoginScreen.tsx
@@ -3,18 +3,13 @@ import {SERVER_BASE_URL, SOCIAL_LOGIN_REDIRECT_URI} from '@env';
 
 const REDIRECT_URI = SOCIAL_LOGIN_REDIRECT_URI;
 
-//
-// type A = {name: string; age: number};
-// type B = {name: string; tall: number};
-//
-// const a: A = {name: 'a'};
-// const a: A = {name: 'a', age: 11, tall: 11};
-
 function KakaoLoginScreen() {
   const handleOnMessage = (event: WebViewMessageEvent) => {
     if (event.nativeEvent.url.includes(`${REDIRECT_URI}?token=`)) {
       const token = event.nativeEvent.url.replace(`${REDIRECT_URI}?token=`, '');
       console.log(token);
+
+      //storage조회
     }
   };
 
@@ -25,7 +20,6 @@ function KakaoLoginScreen() {
       }}
       onMessage={handleOnMessage}
       injectedJavaScript={"window.ReactNativeWebView.postMessage('')"}
-      onError={error => console.error(error)}
     />
   );
 }
