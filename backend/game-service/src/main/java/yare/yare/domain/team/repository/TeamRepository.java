e929c6f459675e930c1512deff33ff36a2cb4f84
박동환 <srpark13@naver.com>
2024-11-07 14:01:42 +0900

    fix : 중복 메서드 단일화

--- backend/game-service/src/main/java/yare/yare/domain/team/repository/TeamRepository.java
+++ backend/game-service/src/main/java/yare/yare/domain/team/repository/TeamRepository.java
@@ -2,7 +2,6 @@ package yare.yare.domain.team.repository;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import yare.yare.domain.team.entity.Team;
 
@@ -14,11 +13,6 @@ public interface TeamRepository extends JpaRepository<Team, Integer> {
 
     @EntityGraph(attributePaths = {"stadium"})
     List<Team> findAllBy();
-
-    @Query("select t " +
-            "from Team t " +
-            "where t.name = :name")
-    Optional<Team> findTeamByName(String name);
 
     Optional<Team> findByName(String name);
 }
