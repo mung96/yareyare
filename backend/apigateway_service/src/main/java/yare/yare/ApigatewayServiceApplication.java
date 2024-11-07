ffb1682aa2f3950f522f07ef355a3961c5486723
이윤지 <yunji383614@gmail.com>
2024-11-07 13:18:15 +0900

    feat : 초기 설정

--- backend/apigateway_service/src/main/java/yare/yare/ApigatewayServiceApplication.java
+++ backend/apigateway_service/src/main/java/yare/yare/ApigatewayServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YareYareApplication {
+public class ApigatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YareYareApplication.class, args);
+		SpringApplication.run(ApigatewayServiceApplication.class, args);
 	}
 
 }
