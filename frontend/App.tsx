37077d349b47ec335f0eec076369abe28c6c65e9
mung96 <mung5079@gmail.com>
2024-11-07 12:14:31 +0900

    feat: react-query설정

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,14 +2,18 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from '@/main/stores/clients/rootReducer.ts';
 import RootNavigation from '@/main/apps/navigations/RootNavigation.tsx';
+import {QueryClientProvider} from '@tanstack/react-query';
+import queryClient from '@/main/apis/queryClient.ts';
 
 const store = createStore(rootReducer);
 
 function App(): React.JSX.Element {
   return (
-    <Provider store={store}>
-      <RootNavigation />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <RootNavigation />
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
