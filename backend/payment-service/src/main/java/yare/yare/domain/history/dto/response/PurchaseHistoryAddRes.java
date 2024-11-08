0dd196fbee53702db7bad727613741c00f0395de
정지연 <jung_j_yeon@naver.com>
2024-11-08 13:10:34 +0900

    feat : 생성자, builder 어노테이션 추가

--- backend/payment-service/src/main/java/yare/yare/domain/history/dto/response/PurchaseHistoryAddRes.java
+++ backend/payment-service/src/main/java/yare/yare/domain/history/dto/response/PurchaseHistoryAddRes.java
@@ -1,8 +1,14 @@
 package yare.yare.domain.history.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PurchaseHistoryAddRes {
     private String idempotencyKey;
 
