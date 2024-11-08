d4bf58beaf4a8a6ca47a2d2dd91a4f59874ef096
정지연 <jung_j_yeon@naver.com>
2024-11-08 16:57:39 +0900

    feat : 나의 예매 내역 조회 API 추가

--- backend/payment-service/src/main/java/yare/yare/domain/payment/controller/PurchaseController.java
+++ backend/payment-service/src/main/java/yare/yare/domain/payment/controller/PurchaseController.java
@@ -1,14 +1,18 @@
 package yare.yare.domain.payment.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import yare.yare.domain.payment.dto.TicketDto;
 import yare.yare.domain.payment.dto.request.PurchaseAddReq;
+import yare.yare.domain.payment.dto.response.CancelReservationListRes;
+import yare.yare.domain.payment.dto.response.ReservationListRes;
 import yare.yare.global.dto.ResponseDto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static yare.yare.global.statuscode.SuccessCode.CREATED;
+import static yare.yare.global.statuscode.SuccessCode.OK;
 
 @RestController
 @RequestMapping("/api/payments")
@@ -18,5 +22,59 @@ public class PurchaseController {
     public ResponseDto<Void> purchaseAdd(@RequestBody PurchaseAddReq purchaseAddReq) {
 
         return ResponseDto.success(CREATED, null);
+    }
+
+    @GetMapping("/tickets/purchases")
+    public ResponseDto<?> reservationList() {
+        ReservationListRes result = new ReservationListRes();
+        List<TicketDto> tickets = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            TicketDto ticket = TicketDto.builder()
+                    .purchaseId((long) i)
+                    .seasonName("2024 신한SOL뱅크 KBO 리그")
+                    .awayTeamName("두산")
+                    .homeTeamName("기아")
+                    .reservationId("T327135"+(i+1))
+                    .reservationDate("2024.06.08")
+                    .stadiumName("잠실야구장")
+                    .gameDateTime("2024.06.13(목) 18:30")
+                    .cancelDeadline("2024.06.12(수) 23:59")
+                    .purchaseStatus("예매완료")
+                    .build();
+
+            tickets.add(ticket);
+        }
+
+        result.setTickets(tickets);
+
+        return ResponseDto.success(OK, result);
+    }
+
+    @GetMapping("/tickets/cancellations")
+    public ResponseDto<?> cancelReservationList() {
+        CancelReservationListRes result = new CancelReservationListRes();
+        List<TicketDto> tickets = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            TicketDto ticket = TicketDto.builder()
+                    .purchaseId((long) i)
+                    .seasonName("2024 신한SOL뱅크 KBO 리그")
+                    .awayTeamName("두산")
+                    .homeTeamName("기아")
+                    .reservationId("T327135"+(i+1))
+                    .reservationDate("2024.06.08")
+                    .stadiumName("잠실야구장")
+                    .gameDateTime("2024.06.13(목) 18:30")
+                    .cancelDeadline("2024.06.12(수) 23:59")
+                    .purchaseStatus("취소완료")
+                    .build();
+
+            tickets.add(ticket);
+        }
+
+        result.setTickets(tickets);
+
+        return ResponseDto.success(OK, result);
     }
 }
--- backend/payment-service/src/main/java/yare/yare/domain/payment/dto/TicketDto.java
+++ backend/payment-service/src/main/java/yare/yare/domain/payment/dto/TicketDto.java
@@ -1,5 +1,14 @@
 package yare.yare.domain.payment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TicketDto {
     private Long purchaseId;
 
