970f477cf642568cfd6351ab1a5ccee7c151c8cb
정지연 <jung_j_yeon@naver.com>
2024-11-11 13:23:18 +0900

    feat : 생성자 추가

--- backend/payment-service/src/main/java/yare/yare/domain/payment/dto/TicketDto.java
+++ backend/payment-service/src/main/java/yare/yare/domain/payment/dto/TicketDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+import static yare.yare.global.formatter.DateFormatter.*;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -29,4 +33,19 @@ public class TicketDto {
     private String cancelDeadline;
 
     private String purchaseStatus;
+
+    public TicketDto(Long purchaseId, String seasonName, String awayTeamName, String homeTeamName,
+                     String reservationId, LocalDateTime createdAt, String stadiumName,
+                     LocalDateTime gameDatetime, Boolean canceled) {
+        this.purchaseId = purchaseId;
+        this.seasonName = seasonName;
+        this.awayTeamName = awayTeamName;
+        this.homeTeamName = homeTeamName;
+        this.reservationId = reservationId;
+        this.reservationDate = convertToReservationDate(createdAt);
+        this.stadiumName = stadiumName;
+        this.gameDateTime = convertToDateTime(gameDatetime);
+        this.cancelDeadline = calculateCancelDate(gameDatetime);
+        this.purchaseStatus = canceled ? "취소완료" : "예매완료";
+    }
 }
