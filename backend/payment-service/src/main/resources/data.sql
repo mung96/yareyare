95620778f1a0e42a12eaf4835a827158d5a54991
정지연 <jung_j_yeon@naver.com>
2024-11-11 13:22:36 +0900

    feat : 테스트용 sql 추가

---
+++ backend/payment-service/src/main/resources/data.sql
@@ -0,0 +1,21 @@
+insert into game(game_id, season_name, away_team_name, home_team_name, stadium_name, game_datetime) values(
+       1, '2024 KBO', '기아', 'LG', '잠실 야구장', now()
+);
+
+insert into purchase(purchase_id, member_id, vendor, created_at, updated_at, idempotency_key, total_price, canceled, reservation_id, game_id, grade_name, is_deleted) values
+       (1, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1f', 12000, false, 'T328204945', 1, '1루 내야', false),
+       (2, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1g', 15000, false, 'T328204947', 1, '1루 내야', false),
+       (3, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1a', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (4, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b12', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (5, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b15', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (6, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b17', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (7, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b10', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (8, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1e', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (9, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1y', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (10, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1o', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (11, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1p', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (12, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1v', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (13, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1n', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (14, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b1m', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (15, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b2m', 13000, false, 'T328204946', 1, '1루 내야', false),
+       (16, 1, 'CARD', now(), now(), '85252273-0072-42a0-a04e-cc5cf6373b32', 13000, false, 'T328204946', 1, '1루 내야', false);
