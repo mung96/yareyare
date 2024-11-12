93dcc8255d0f04bfcf60aa92de46393088259e87
mung96 <mung5079@gmail.com>
2024-11-12 13:39:17 +0900

    feat: webview portone 설정

---
+++ webview/src/certificate/Certification.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Certification() {
+  function onClickCertification() {
+    const userCode = 'imp00000000';
+
+    /* 2. 본인인증 데이터 정의하기 */
+    const data = {
+      merchant_uid: `mid_${new Date().getTime()}`,  // 주문번호
+      company: '아임포트',                           // 회사명 또는 URL
+      carrier: 'SKT',                              // 통신사
+      name: '홍길동',                                // 이름
+      phone: '01012341234',                        // 전화번호
+    };
+      /* 5. 리액트 네이티브 환경에 대응하기 */
+      const params = {
+        userCode,                                   // 가맹점 식별코드
+        data,                                       // 본인인증 데이터
+        type: 'certification',                      // 결제와 본인인증 구분을 위한 필드
+      };
+      const paramsToString = JSON.stringify(params);
+      window.ReactNativeWebView.postMessage(paramsToString);
+  }
+
+  return <button onClick={onClickCertification}>본인인증 하기</button>
+}
+export default Certification;
