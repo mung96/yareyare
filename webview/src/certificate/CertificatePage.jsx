39a59bebdc8d123fc4b67daddab44eb39bdeb497
mung96 <mung5079@gmail.com>
2024-11-12 12:49:06 +0900

    feat: 본인인증 페이지 추가

---
+++ webview/src/certificate/CertificatePage.jsx
@@ -0,0 +1,10 @@
+import {fetchPortOne} from "../payment/Payment";
+
+function CertificatePage() {
+  return (
+      <div>
+      </div>
+  );
+}
+
+export default CertificatePage;
---
+++ webview/src/certificate/CertificateRedirectPage.jsx
@@ -0,0 +1,10 @@
+import {fetchPortOne} from "../payment/Payment";
+
+function CertificateRedirectPage() {
+  return (
+      <div>
+      </div>
+  );
+}
+
+export default CertificateRedirectPage;
