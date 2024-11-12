78418a744c95f00b693bfe5db43ea35cd2e9a363
mung96 <mung5079@gmail.com>
2024-11-12 12:09:24 +0900

    design 경기일정 캘린더 구현

--- frontend/src/main/apps/screens/GameScheduleScreen.tsx
+++ frontend/src/main/apps/screens/GameScheduleScreen.tsx
@@ -1,9 +1,11 @@
 import {Pressable, SafeAreaView, StyleSheet, View} from 'react-native';
 import CustomText from '@/main/ui/widgets/CustomText.tsx';
 import {COLORS} from '@/main/shared/styles';
-import {getGameSchedule} from '@/main/apis/game.ts';
 import Icon from 'react-native-vector-icons/AntDesign';
 import useCalendar from '@/main/services/hooks/useCalendar.ts';
+import {useGameScheduleQuery} from '@/main/services/hooks/queries/useGameScheduleQuery.ts';
+import useGameScheduleModel from '@/main/services/hooks/useGameScheduleModel.ts';
+import {SvgUri} from 'react-native-svg';
 const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
 function Calendar() {
@@ -14,21 +16,14 @@ function Calendar() {
     handleNextMonth,
     calendarDays,
   } = useCalendar();
+  //TODO: teamId에 myteam을 넣어야함
+  const {gameSchedule} = useGameScheduleModel(
+    '1',
+    String(currentYear),
+    String(currentMonth + 1),
+  );
 
-  // const fetch = async () => {
-  //   try {
-  //     const response = await getGameSchedule(
-  //       '1',
-  //       String(currentYear),
-  //       String(currentMonth + 1),
-  //     );
-  //     console.log(response);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  // fetch();
-
+  console.log(gameSchedule);
   return (
     <SafeAreaView
       style={{
@@ -82,12 +77,61 @@ function Calendar() {
         <View style={styles.calendar}>
           {calendarDays.map((day, index) => (
             <View style={[styles.date]}>
-              <View>
-                <CustomText key={index}>{day}</CustomText>
-              </View>
+              {gameSchedule[day] ? (
+                <>
+                  <View
+                    style={[
+                      styles.timeBox,
+                      gameSchedule[day].isHome && styles.homeBox,
+                    ]}>
+                    <CustomText
+                      key={index}
+                      style={[
+                        styles.timeText,
+                        gameSchedule[day].isHome && styles.homeText,
+                      ]}>
+                      {day}
+                    </CustomText>
+                  </View>
+                  <View style={styles.content}>
+                    <SvgUri
+                      uri={gameSchedule[day].opponentTeamLogo}
+                      width={24}
+                      height={24}
+                    />
+                    <CustomText
+                      key={index}
+                      style={[
+                        styles.description,
+                        gameSchedule[day].description === 'WIN' &&
+                          styles.descriptionWin,
+                      ]}>
+                      {gameSchedule[day].description}
+                    </CustomText>
+                    <View
+                      style={[
+                        styles.timeBox,
+                        gameSchedule[day].isHome && styles.homeBox,
+                      ]}>
+                      <CustomText
+                        key={index}
+                        style={[
+                          styles.timeText,
+                          gameSchedule[day].isHome && styles.homeText,
+                        ]}>
+                        {gameSchedule[day].startTime}
+                      </CustomText>
+                    </View>
+                  </View>
+                </>
+              ) : (
+                <View style={[styles.timeBox]}>
+                  <CustomText key={index} style={[styles.timeText]}>
+                    {day}
+                  </CustomText>
+                </View>
+              )}
               <View />
-              <CustomText key={index}>WIN</CustomText>
-              <CustomText key={index}>{day}</CustomText>
             </View>
           ))}
         </View>
@@ -133,16 +177,55 @@ const styles = StyleSheet.create({
   },
   date: {
     width: 45,
-    textAlign: 'center',
-    height: 90,
+    height: 85,
     borderWidth: 1,
     borderColor: COLORS.GRAY_200,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   schedule: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
-    padding: 10,
+  },
+
+  description: {
+    fontSize: 14,
+    color: COLORS.GRAY_300,
+    fontWeight: 'bold',
+    width: '100%',
+  },
+  descriptionWin: {
+    color: COLORS.PURPLE_100,
+    width: '100%',
+  },
+  timeBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: COLORS.GRAY_100,
+    width: '100%',
+  },
+  timeText: {
+    width: '100%',
+    color: COLORS.GRAY_300,
+    textAlign: 'center',
+    lineHeight: 20,
+  },
+  homeBox: {
+    backgroundColor: COLORS.PURPLE_500,
+    width: '100%',
+  },
+  homeText: {
+    color: COLORS.WHITE,
   },
 });
 
