573f4cd2c1640ff56e5619e83e05ad156865d739
mung96 <mung5079@gmail.com>
2024-11-13 15:10:45 +0900

    design: 팀변경 레이아웃 구현

--- frontend/src/main/apps/screens/mypage/UpdateTeamScreen.tsx
+++ frontend/src/main/apps/screens/mypage/UpdateTeamScreen.tsx
@@ -1,45 +1,56 @@
 import CustomText from '@/main/ui/widgets/CustomText.tsx';
 import {useTeamQuery} from '@/main/services/hooks/queries/useTeamQuery.ts';
 import {SvgUri} from 'react-native-svg';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {COLORS} from '@/main/shared/styles';
 
 function UpdateTeamScreen() {
   const {data: teamListData} = useTeamQuery();
+  console.log(teamListData);
   return (
-    <View style={styles.layout}>
+    <ScrollView contentContainerStyle={styles.layout}>
       {teamListData?.teams.map(team => (
         <View style={styles.logoContainer}>
           <View style={styles.logoBox}>
-            <SvgUri uri={team.teamLogo} width={96} height={96} />
+            <SvgUri uri={team.teamLogo} width={90} height={90} />
           </View>
-          <CustomText>{team.teamName}</CustomText>
+          <CustomText style={styles.nameText}>{team.teamName}</CustomText>
         </View>
       ))}
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   layout: {
-    borderWidth: 1,
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    paddingTop: 24,
+    paddingTop: 12,
     alignItems: 'center',
     justifyContent: 'flex-start',
     paddingHorizontal: 21,
     gap: 12,
-    height: '100%',
     backgroundColor: COLORS.WHITE,
   },
   logoContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     width: '30%',
   },
   logoBox: {
     width: '100%',
-    borderWidth: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: COLORS.GRAY_400,
+    borderRadius: 50,
+  },
+  nameText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: COLORS.BLACK,
   },
 });
 
