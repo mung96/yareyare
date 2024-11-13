4b726b7d75c25d7c8699b046a531ebf0b8db98a2
mung96 <mung5079@gmail.com>
2024-11-13 11:02:37 +0900

    design: 팀변경 화면 레이아웃 디자인

--- frontend/src/main/apps/screens/mypage/UpdateTeamScreen.tsx
+++ frontend/src/main/apps/screens/mypage/UpdateTeamScreen.tsx
@@ -1,12 +1,46 @@
-import CommonLayout from '@/main/apps/layout/CommonLayout.tsx';
 import CustomText from '@/main/ui/widgets/CustomText.tsx';
+import {useTeamQuery} from '@/main/services/hooks/queries/useTeamQuery.ts';
+import {SvgUri} from 'react-native-svg';
+import {StyleSheet, View} from 'react-native';
+import {COLORS} from '@/main/shared/styles';
 
 function UpdateTeamScreen() {
+  const {data: teamListData} = useTeamQuery();
   return (
-    <CommonLayout>
-      <CustomText>나의 팀 변경</CustomText>
-    </CommonLayout>
+    <View style={styles.layout}>
+      {teamListData?.teams.map(team => (
+        <View style={styles.logoContainer}>
+          <View style={styles.logoBox}>
+            <SvgUri uri={team.teamLogo} width={96} height={96} />
+          </View>
+          <CustomText>{team.teamName}</CustomText>
+        </View>
+      ))}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  layout: {
+    borderWidth: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingTop: 24,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 21,
+    gap: 12,
+    height: '100%',
+    backgroundColor: COLORS.WHITE,
+  },
+  logoContainer: {
+    width: '30%',
+  },
+  logoBox: {
+    width: '100%',
+    borderWidth: 1,
+  },
+});
 
 export default UpdateTeamScreen;
