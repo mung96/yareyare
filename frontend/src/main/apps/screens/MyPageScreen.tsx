86970435e096d3f1e4568071846d36013fbfef74
mung96 <mung5079@gmail.com>
2024-11-13 09:59:19 +0900

    design: 마일리지, 쿠폰 박스 디자인

--- frontend/src/main/apps/screens/MyPageScreen.tsx
+++ frontend/src/main/apps/screens/MyPageScreen.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import MyPageList from '@/main/ui/components/member/MyPageList.tsx';
+import {ScrollView} from 'react-native';
+import {COLORS} from '@/main/shared/styles';
+import CouponBox from '@/main/ui/components/member/CouponBox.tsx';
 
 function MyPageScreen() {
-  return <MyPageList />;
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        paddingHorizontal: 12,
+        paddingTop: 12,
+        paddingBottom: 64,
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 16,
+        width: '100%',
+        backgroundColor: COLORS.WHITE,
+      }}>
+      <CouponBox />
+      <MyPageList />
+    </ScrollView>
+  );
 }
 
 export default MyPageScreen;
