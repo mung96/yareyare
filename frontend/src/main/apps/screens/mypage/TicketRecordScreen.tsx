91fa6e7e4ac6c05ab4d4c4b846f4f8312102d020
mung96 <mung5079@gmail.com>
2024-11-13 12:09:07 +0900

    design: 티켓예매내역 레이아웃 구현

--- frontend/src/main/apps/screens/mypage/TicketRecordScreen.tsx
+++ frontend/src/main/apps/screens/mypage/TicketRecordScreen.tsx
@@ -2,19 +2,29 @@ import CommonLayout from '@/main/apps/layout/CommonLayout.tsx';
 import CustomText from '@/main/ui/widgets/CustomText.tsx';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {MypageParamList} from '@/main/apps/navigations/MypageNavigation.tsx';
-import {View} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import TicketRecordItem from '@/main/ui/components/member/TicketRecordItem.tsx';
+import {COLORS} from '@/main/shared/styles';
 
 function TicketRecordScreen({
   route,
 }: NativeStackScreenProps<MypageParamList, 'TicketRecord'>) {
   const {type: recordType} = route.params;
   return (
-    <CommonLayout>
-      {recordType === 'reserve' && <CustomText>티켓 예매 내역</CustomText>}
-      {recordType === 'cancel' && <CustomText>티켓 취소 내역</CustomText>}
+    <ScrollView
+      contentContainerStyle={{
+        paddingHorizontal: 12,
+        paddingTop: 12,
+        alignItems: 'center',
+        gap: 12,
+        width: '100%',
+        backgroundColor: COLORS.WHITE,
+      }}>
       <TicketRecordItem />
-    </CommonLayout>
+      <TicketRecordItem />
+      <TicketRecordItem />
+      <TicketRecordItem />
+    </ScrollView>
   );
 }
 
