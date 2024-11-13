6a2ba35ce95e14717103b09f521df642a476208b
mung96 <mung5079@gmail.com>
2024-11-13 11:59:21 +0900

    design: 예매내역 item 디자인

--- frontend/src/main/apps/screens/mypage/TicketRecordScreen.tsx
+++ frontend/src/main/apps/screens/mypage/TicketRecordScreen.tsx
@@ -2,6 +2,8 @@ import CommonLayout from '@/main/apps/layout/CommonLayout.tsx';
 import CustomText from '@/main/ui/widgets/CustomText.tsx';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {MypageParamList} from '@/main/apps/navigations/MypageNavigation.tsx';
+import {View} from 'react-native';
+import TicketRecordItem from '@/main/ui/components/member/TicketRecordItem.tsx';
 
 function TicketRecordScreen({
   route,
@@ -11,6 +13,7 @@ function TicketRecordScreen({
     <CommonLayout>
       {recordType === 'reserve' && <CustomText>티켓 예매 내역</CustomText>}
       {recordType === 'cancel' && <CustomText>티켓 취소 내역</CustomText>}
+      <TicketRecordItem />
     </CommonLayout>
   );
 }
