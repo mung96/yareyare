bd349cca94346e3b996d828fecf20a73ae6f6a65
mung96 <mung5079@gmail.com>
2024-11-14 19:52:50 +0900

    feat: 리다이렉트 페이지에서 홈으로 이동 기능 구현

--- frontend/src/main/apps/screens/reservationProcess/PortOneRedirectScreen.tsx
+++ frontend/src/main/apps/screens/reservationProcess/PortOneRedirectScreen.tsx
@@ -1,7 +1,24 @@
-import {Text} from 'react-native';
+import {Alert, Image} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {moveNavigation} from '@/main/stores/navigationCategory.ts';
 
 function PortOneRedirectScreen({}) {
-  return <Text>aaa</Text>;
+  const dispatch = useDispatch();
+  Alert.alert('알림', '아직 개발 중이에요. 다른 방법으로 로그인해주세요', [
+    {
+      text: '결제가 완료되었습니다',
+      onPress: () => {
+        dispatch(moveNavigation('navbar'));
+      },
+      style: 'destructive',
+    },
+  ]);
+  return (
+    <Image
+      source={require('@/main/assets/yareLogo.png')}
+      resizeMode={'contain'}
+    />
+  );
 }
 
 export default PortOneRedirectScreen;
