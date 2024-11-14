9ba4c0b95291c018aee13377181f4452f28cd668
정지연 <jung_j_yeon@naver.com>
2024-11-14 21:49:30 +0900

    feat : 디버깅용 로그 추가

--- backend/payment-service/src/main/java/yare/yare/domain/portone/service/PortOneServiceImpl.java
+++ backend/payment-service/src/main/java/yare/yare/domain/portone/service/PortOneServiceImpl.java
@@ -1,6 +1,7 @@
 package yare.yare.domain.portone.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import yare.yare.domain.portone.dto.PortOnePriceRes;
@@ -10,6 +11,7 @@ import yare.yare.domain.portone.dto.PortOneTokenRes;
 import yare.yare.domain.portone.feign_client.PortOneFeignClientCustom;
 import yare.yare.global.utils.RedisUtils;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PortOneServiceImpl implements PortOneService {
@@ -25,22 +27,25 @@ public class PortOneServiceImpl implements PortOneService {
     @Override
     public Integer getPrice(String paymentId) {
         String token = createToken();
-
+        log.info("token %{}", token);
         PortOnePriceRes result = portOneFeignClientCustom.getPrice(PAYMENT_PREFIX+paymentId, token);
-
+        log.info("result %{}", result.getResponse().getAmount().getTotal());
         return result.getResponse().getAmount().getTotal();
     }
 
     private String createToken() {
-        String token;
+        String token = (String) redisUtils.getData("portOneAccessToken");
 
-        if((token = (String) redisUtils.getData("portOneAccessToken")) == null) {
-            String refreshToken;
+        if(token == null) {
+            String refreshToken = (String) redisUtils.getData("portOneRefreshToken");
 
-            if((refreshToken = (String) redisUtils.getData("portOneRefreshToken")) == null) {
+            if(refreshToken == null) {
                 PortOneTokenReq portOneTokenReq = new PortOneTokenReq(apiSecret);
 
                 PortOneTokenRes newToken = portOneFeignClientCustom.getToken(portOneTokenReq);
+
+                log.info(newToken.getAccess_token());
+                log.info(newToken.getRefresh_token());
 
                 redisUtils.setDataWithExpiration("portOneAccessToken", newToken.getAccess_token(), 86300L);
                 redisUtils.setDataWithExpiration("portOneRefreshToken", newToken.getRefresh_token(), 604000L);
