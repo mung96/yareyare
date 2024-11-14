4bfba155b11ff45835c94df776129b4ec5e48ed0
정지연 <jung_j_yeon@naver.com>
2024-11-14 11:05:21 +0900

    feat : repository 어노테이션 추가

--- backend/payment-service/src/main/java/yare/yare/domain/payment/repository/PurchaseRepository.java
+++ backend/payment-service/src/main/java/yare/yare/domain/payment/repository/PurchaseRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import yare.yare.domain.history.entity.PurchaseHistory;
+import org.springframework.stereotype.Repository;
 import yare.yare.domain.payment.dto.TicketDto;
 import yare.yare.domain.payment.entity.Purchase;
 
+@Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
     @Query("SELECT new yare.yare.domain.payment.dto.TicketDto(" +
             "p.id, g.seasonName, g.awayTeamName, g.homeTeamName, p.reservationId," +
