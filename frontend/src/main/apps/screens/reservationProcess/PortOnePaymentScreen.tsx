a7f662d822d3cb5f7259fb8983e0887eadd09029
mung96 <mung5079@gmail.com>
2024-11-14 17:53:43 +0900

    feat: 결제 응답 가공 로직 추가

--- frontend/src/main/apps/screens/reservationProcess/PortOnePaymentScreen.tsx
+++ frontend/src/main/apps/screens/reservationProcess/PortOnePaymentScreen.tsx
@@ -1,9 +1,12 @@
 import WebView, {WebViewMessageEvent} from 'react-native-webview';
-import {WEB_VIEW_SERVER} from '@env';
+import {PORTONE_REDIRECT_URI, WEB_VIEW_SERVER} from '@env';
 import {useRef} from 'react';
 import useMemberModel from '@/main/services/hooks/useMemberModel.ts';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {ReservationParamList} from '@/main/apps/navigations/ReservationNavigation.tsx';
+import {apiRequester} from '@/main/apis/requester.ts';
+import {setEncryptStorage} from '@/main/shared/utils/encryptStorage.ts';
+import {login} from '@/main/stores/member.ts';
 
 function PortOnePaymentScreen({
   route,
@@ -28,6 +31,17 @@ function PortOnePaymentScreen({
   };
   const handleOnMessage = async (event: WebViewMessageEvent) => {
     console.log(event.nativeEvent);
+    if (event.nativeEvent.url.includes(`${PORTONE_REDIRECT_URI}?`)) {
+      const queryString = event.nativeEvent.url.split('?')[1].split('&');
+      const paymentInfo = {
+        transactionType: queryString[1].split('=')[1],
+        paymentId: queryString[3].split('=')[1],
+        txId: queryString[5].split('=')[1],
+      };
+      console.log(queryString);
+      console.log(paymentInfo);
+      // const response = await apiRequester.get(`members/token/${token}`);
+    }
   };
 
   return (
