e849c32e61dd3e1915c80ebff468aaa1fc8713c6
Cutaku <mmzz0219@gmail.com>
2024-11-14 21:59:16 +0900

    feat: init project

---
+++ backend/waiting-service/src/test/java/yare/yare/WaitingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yare.yare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
