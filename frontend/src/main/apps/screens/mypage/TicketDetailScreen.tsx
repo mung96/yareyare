9b61be2ffe92a3dcd9e9f2a9baed5953dd496da7
mung96 <mung5079@gmail.com>
2024-11-17 11:42:41 +0900

    feat: 페이지 이동시 purchaseId 전달 기능 구현

--- frontend/src/main/apps/screens/mypage/TicketDetailScreen.tsx
+++ frontend/src/main/apps/screens/mypage/TicketDetailScreen.tsx
@@ -1,6 +1,11 @@
 import {Text} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {MypageParamList} from '@/main/apps/navigations/MypageNavigation.tsx';
 
-function TicketDetailScreen() {
+function TicketDetailScreen({
+  route,
+}: NativeStackScreenProps<MypageParamList, 'TicketDetail'>) {
+  console.log(route.params.purchaseId);
   return <Text>ticketDetail</Text>;
 }
 
