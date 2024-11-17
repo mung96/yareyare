609428fa6f4e762915dc096ac930206b7cc792b8
mung96 <mung5079@gmail.com>
2024-11-17 12:02:11 +0900

    feat: 티켓 구매내역 400 에러 처리 구현

--- frontend/src/main/apps/screens/mypage/TicketDetailScreen.tsx
+++ frontend/src/main/apps/screens/mypage/TicketDetailScreen.tsx
@@ -1,12 +1,37 @@
-import {Text} from 'react-native';
+import {Alert, Text} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {MypageParamList} from '@/main/apps/navigations/MypageNavigation.tsx';
+import {useGetTicketDetailQuery} from '@/main/services/hooks/queries/useTicketRecordQuery.ts';
+import {PATH} from '@/main/shared/constants';
+import {isAxiosError} from 'axios';
 
 function TicketDetailScreen({
   route,
+  navigation,
 }: NativeStackScreenProps<MypageParamList, 'TicketDetail'>) {
   console.log(route.params.purchaseId);
-  return <Text>ticketDetail</Text>;
+
+  const {data: ticketDetailData, error} = useGetTicketDetailQuery(
+    route.params.purchaseId,
+  );
+
+  if (isAxiosError(error)) {
+    Alert.alert('', '해당 구매 내역이 존재하지 않아요', [
+      {
+        text: '돌아가기',
+        onPress: () => {
+          navigation.pop();
+        },
+        style: 'destructive',
+      },
+    ]);
+  }
+  return (
+    <Text
+      onPress={() => navigation.navigate(PATH.TICKET_IMAGE, ticketDetailData!)}>
+      ticketDetail
+    </Text>
+  );
 }
 
 export default TicketDetailScreen;
