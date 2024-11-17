110dca269d70cdaab7c468fdd7e117acd955ed56
jeongjiyeon315 <jung_j_yeon@naver.com>
2024-11-18 00:47:24 +0900

    feat : 생성자 어노테이션 추가

--- backend/waiting-service/src/main/java/yare/yare/domain/waiting/dto/JoinQueueReq.java
+++ backend/waiting-service/src/main/java/yare/yare/domain/waiting/dto/JoinQueueReq.java
@@ -1,8 +1,12 @@
 package yare.yare.domain.waiting.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class JoinQueueReq {
 
     private Long gameId;
