dad128e1a02a70ca08cdb4713d7c471515572fa0
mung96 <mung5079@gmail.com>
2024-11-17 12:03:39 +0900

    feat: 티켓 이미지 페이지로 response 전달 구현

--- frontend/src/main/apps/screens/mypage/TicketImageScreen.tsx
+++ frontend/src/main/apps/screens/mypage/TicketImageScreen.tsx
@@ -1,7 +1,12 @@
 import {Text} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {MypageParamList} from '@/main/apps/navigations/MypageNavigation.tsx';
 
-function TicketImageScreen() {
-  return <Text>ticketDetail</Text>;
+function TicketImageScreen({
+  route,
+}: NativeStackScreenProps<MypageParamList, 'TicketImage'>) {
+  console.log(route.params);
+  return <Text>티켓 이미지</Text>;
 }
 
 export default TicketImageScreen;
