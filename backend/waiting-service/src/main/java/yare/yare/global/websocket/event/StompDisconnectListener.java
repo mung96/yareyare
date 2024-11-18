7435bbedf0ce4745eba76db63c18eb96e2aeed32
박동환 <srpark13@naver.com>
2024-11-18 13:51:25 +0900

    feat : websocket 이탈 로깅 추가

--- backend/waiting-service/src/main/java/yare/yare/global/websocket/event/StompDisconnectListener.java
+++ backend/waiting-service/src/main/java/yare/yare/global/websocket/event/StompDisconnectListener.java
@@ -1,10 +1,12 @@
 package yare.yare.global.websocket.event;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class StompDisconnectListener {
@@ -13,7 +15,7 @@ public class StompDisconnectListener {
     @EventListener
     public void handleSessionDisconnect(SessionDisconnectEvent event) {
         String sessionId = event.getSessionId();
-
+        log.info("disconnect websocket sessionId={}", sessionId);
         sessionManager.removeSessionById(sessionId);
     }
 }
