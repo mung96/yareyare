e9df4ff1722f0bf91c6b322bbcf70203f6f417bb
mung96 <mung5079@gmail.com>
2024-11-19 12:45:44 +0900

    feat: 웹뷰 로딩 컴포넌트 구현

--- webview/src/App.js
+++ webview/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import yare from './yareLogo.png'
+import LoadingComponent from "./LodingComponent";
 
 function App() {
   return (
       <div id={'container'}>
         <img src={yare} alt={'logo'}/>
+        <LoadingComponent/>
         <h1>로그인 중입니다.</h1>
       </div>
   );
---
+++ webview/src/LodingComponent.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef } from 'react';
+import './loding.css';
+
+function LoadingComponent() {
+  const dot1 = useRef<HTMLDivElement | null>(null);
+  const dot2 = useRef<HTMLDivElement | null>(null);
+  const dot3 = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const animateDots = () => {
+      const keyframes = [
+        { transform: 'translateY(0)' },
+        { transform: 'translateY(-20px)' },
+        { transform: 'translateY(0)' },
+      ];
+
+      const options = {
+        duration: 300,
+        easing: 'ease-in-out',
+      };
+
+      if (dot1.current) {
+        dot1.current.animate(keyframes, options);
+      }
+      setTimeout(() => {
+        if (dot2.current) {
+          dot2.current.animate(keyframes, options);
+        }
+      }, 200);
+      setTimeout(() => {
+        if (dot3.current) {
+          dot3.current.animate(keyframes, options);
+        }
+      }, 400);
+
+      setTimeout(animateDots, 2000);
+    };
+
+    animateDots();
+  }, []);
+
+  return (
+      <div className="container">
+        <div className="loadingBox">
+          <div ref={dot1} className="dot" />
+          <div ref={dot2} className="dot" />
+          <div ref={dot3} className="dot" />
+        </div>
+        <p>잠시만 기다려주세요.</p>
+      </div>
+  );
+}
+
+export default LoadingComponent;
