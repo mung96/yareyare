c00706ebc2a96bc1b7b50988166b06bce153323f
정지연 <jung_j_yeon@naver.com>
2024-11-19 17:35:44 +0900

    feat : portone api 토큰 받아오는 방식 변경

--- backend/payment-service/src/main/java/yare/yare/domain/portone/service/PortOneServiceImpl.java
+++ backend/payment-service/src/main/java/yare/yare/domain/portone/service/PortOneServiceImpl.java
@@ -40,34 +40,9 @@ public class PortOneServiceImpl implements PortOneService {
     }
 
     private String createToken() {
-        String token = (String) redisUtils.getData("portOneAccessToken");
+        PortOneTokenReq portOneTokenReq = new PortOneTokenReq(apiSecret);
 
-        if(token == null) {
-            String refreshToken = (String) redisUtils.getData("portOneRefreshToken");
-
-            if(refreshToken == null) {
-                PortOneTokenReq portOneTokenReq = new PortOneTokenReq(apiSecret);
-
-                PortOneTokenRes newToken = portOneFeignClientCustom.getToken(portOneTokenReq);
-
-                redisUtils.setDataWithExpiration("portOneAccessToken", newToken.getAccessToken(), 3600L);
-                redisUtils.setDataWithExpiration("portOneRefreshToken", newToken.getRefreshToken(), 4000L);
-
-                return TOKEN_PREFIX + newToken.getAccessToken();
-            }
-
-            return newAccessToken(refreshToken);
-        }
-
-        return TOKEN_PREFIX + token;
-    }
-
-    private String newAccessToken(String refreshToken) {
-        PortOneRefreshTokenReq req = new PortOneRefreshTokenReq(refreshToken);
-        PortOneTokenRes newToken = portOneFeignClientCustom.refreshToken(req);
-
-        redisUtils.setDataWithExpiration("portOneAccessToken", newToken.getAccessToken(), 3600L);
-        redisUtils.setDataWithExpiration("portOneRefreshToken", newToken.getRefreshToken(), 4000L);
+        PortOneTokenRes newToken = portOneFeignClientCustom.getToken(portOneTokenReq);
 
         return TOKEN_PREFIX + newToken.getAccessToken();
     }
